de4bafe1484ca380467692b8918b058f96a85518
ianrios <ian.rios1a@gmail.com>
2019-08-22 16:00:25 -0400

    fixed useless constructor

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,9 +5,6 @@ import TileGenerator from "../../utils/TileGenerator";
 import Board from "../Board/Board";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
   componentDidMount() {
     const tiles = new TileGenerator();
     tiles.getTileStatus();
